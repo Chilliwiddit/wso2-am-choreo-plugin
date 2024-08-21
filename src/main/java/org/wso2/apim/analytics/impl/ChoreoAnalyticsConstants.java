0604eaa151535f89418d80e976395ea81a983b84
Chilliwiddit <nivedhith@wso2.com>
2024-08-21 16:25:23 +0530

    Create constant class

---
+++ src/main/java/org/wso2/apim/analytics/impl/ChoreoAnalyticsConstants.java
@@ -0,0 +1,14 @@
+package org.wso2.apim.analytics.impl;
+
+public class ChoreoAnalyticsConstants {
+
+    /**
+     * Choreo based analytics related constants
+     **/
+    public static final String TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    public static final String TIME_ZONE = "UTC";
+    public static final String TIMEZONE_FORMAT = ".023+00:00";
+    public static final String FROM = "from";
+    public static final String TO = "to";
+
+}
