f0decc4bd0e7756618c85989137b6f1c6dfc4737
Chilliwiddit <nivedhith@wso2.com>
2024-08-22 16:35:41 +0530

    Add constants variables

--- src/main/java/org/wso2/apim/analytics/impl/ChoreoAnalyticsConstants.java
+++ src/main/java/org/wso2/apim/analytics/impl/ChoreoAnalyticsConstants.java
@@ -10,5 +10,11 @@ public class ChoreoAnalyticsConstants {
     public static final String TIMEZONE_FORMAT = ".023+00:00";
     public static final String FROM = "from";
     public static final String TO = "to";
+    public static final String API_ID_COL = "apiIds";
+    public static final String TENANT_DOMAIN_COL = "tenantDomains";
+    public static final String TIME_FILTER = "timeFilter";
+    public static final String API_USAGE_BY_APP_FILTER = "successAPIUsageByAppFilter";
+    public static final String ON_PREM_KEY = "onPremKey";
+    public static final String AT = "@";
 
 }
