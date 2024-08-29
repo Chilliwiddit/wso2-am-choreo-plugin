d24ff4bd5af7bd0eaa185bb57ea8059c57198fe0
Chilliwiddit <nivedhith@wso2.com>
2024-08-29 14:29:44 +0530

    Add new constant variables

--- src/main/java/org/wso2/apim/analytics/impl/ChoreoAnalyticsConstants.java
+++ src/main/java/org/wso2/apim/analytics/impl/ChoreoAnalyticsConstants.java
@@ -17,5 +17,15 @@ public class ChoreoAnalyticsConstants {
     public static final String ON_PREM_KEY = "onPremKey";
     public static final String AT = "@";
     public static final String PRODUCTS = "products";
-
+    public static final String COUNT = "count";
+    public static final String GET_USAGE_BY_APPLICATION = "getSuccessAPIsUsageByApplications";
+    public static final String GET_USAGE_BY_APPLICATION_WITH_ON_PREM_KEY = "getSuccessAPIsUsageByApplicationsWithOnPremKey";
+    public static final String COMPLETED = "COMPLETED";
+    public static final String SUCCESSFULL = "SUCCESSFULL";
+    public static final String API_NAME = "apiName";
+    public static final String API_VERSION = "apiVersion";
+    public static final String API_UUID = "apiId";
+    public static final String TENANT_DOMAIN = "apiCreatorTenantDomain";
+    public static final String APPLICATION_NAME = "applicationName";
+    public static final String APPLICATION_OWNER = "applicationOwner";
 }
